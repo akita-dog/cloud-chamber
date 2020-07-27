b3b43b275a1f389e898caf3585ae52a9b74e2486
heyong <hey010@foresealife.com>
2020-07-27 18:03:26 +0800

    nacos动态配置

--- particles/provider-demo1/src/main/java/com/akita/cloudchamber/particles/service/impl/ProviderServiceImpl.java
+++ particles/provider-demo1/src/main/java/com/akita/cloudchamber/particles/service/impl/ProviderServiceImpl.java
@@ -2,11 +2,13 @@ package com.akita.cloudchamber.particles.service.impl;
 
 import com.akita.cloudchamber.particles.service.ProviderService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 @Service
+@RefreshScope
 public class ProviderServiceImpl implements ProviderService {
-    @Value("${test.val}")
+    @Value("${nacos.test.val}")
     private String testConfigVal;
 
     @Override
